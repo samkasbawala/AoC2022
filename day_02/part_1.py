9966a8fa978b5b5376dd2d693cfb819e1c30c039
samkasbawala <samarth.kasbawala@uconn.edu>
2022-12-03 00:53:05 -0500

    day 2

---
+++ day_02/part_1.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+from typing import List
+
+import os
+
+import pytest
+
+INPUT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+INPUT_SAMPLE = """\
+A Y
+B X
+C Z
+"""
+
+EXPECTED = 15
+
+
+def solve(input: str) -> int:
+    games: List[str] = [game for game in input.split("\n") if len(game) > 0]
+
+    win = {"A": "Y", "B": "Z", "C": "X"}
+    pts = {"X": 1, "Y": 2, "Z": 3}
+
+    total = 0
+    for game in games:
+        elf, me = game.split(" ")
+
+        # We win
+        if win[elf] == me:
+            total += 6
+
+        # Tie
+        elif ord(elf) + 23 == ord(me):
+            total += 3
+
+        total += pts[me]
+
+    return total
+
+
+@pytest.mark.parametrize(
+    ("input_sample", "expected"),
+    ((INPUT_SAMPLE, EXPECTED),),
+)
+def test(input_sample: str, expected: int):
+    assert solve(input_sample) == expected
+
+
+def main() -> None:
+    with open(INPUT, "r") as file:
+        print(solve(file.read()))
+
+
+if __name__ == "__main__":
+    main()
---
+++ day_02/part_2.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+from typing import List
+
+import os
+
+import pytest
+
+INPUT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+INPUT_SAMPLE = """\
+A Y
+B X
+C Z
+"""
+
+EXPECTED = 12
+
+
+def solve(input: str) -> int:
+    games: List[str] = [game for game in input.split("\n") if len(game) > 0]
+
+    win = {"A": "Y", "B": "Z", "C": "X"}
+    lose = {"A": "Z", "B": "X", "C": "Y"}
+    pts = {"X": 1, "Y": 2, "Z": 3}
+
+    total = 0
+    for game in games:
+        elf, res = game.split(" ")
+
+        # Tie
+        if res == "Y":
+            total += 3
+            me = chr(ord(elf) + 23)
+
+        # Win
+        elif res == "Z":
+            total += 6
+            me = win[elf]
+
+        # Lose
+        else:
+            me = lose[elf]
+
+        total += pts[me]
+
+    return total
+
+
+@pytest.mark.parametrize(
+    ("input_sample", "expected"),
+    ((INPUT_SAMPLE, EXPECTED),),
+)
+def test(input_sample: str, expected: int):
+    assert solve(input_sample) == expected
+
+
+def main() -> None:
+    with open(INPUT, "r") as file:
+        print(solve(file.read()))
+
+
+if __name__ == "__main__":
+    main()
