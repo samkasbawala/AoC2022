c14df49e99e84d4a1030cd05efc891ab8de09826
samkasbawala <samarth.kasbawala@uconn.edu>
2022-12-03 00:11:09 -0500

    day 01

---
+++ day_01/part_1.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+from typing import List
+
+import os
+
+import pytest
+
+INPUT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+INPUT_SAMPLE = """\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+
+EXPECTED = 24000
+
+
+def solve(input: str) -> int:
+    elves: List[str] = [elf for elf in input.split("\n\n")]
+    cals: List[int] = [sum(int(cal) for cal in elf.splitlines()) for elf in elves]
+
+    return max(cals)
+
+
+@pytest.mark.parametrize(
+    ("input_sample", "expected"),
+    ((INPUT_SAMPLE, EXPECTED),),
+)
+def test(input_sample: str, expected: int):
+    assert solve(input_sample) == expected
+
+
+def main() -> None:
+    with open(INPUT, "r") as file:
+        print(solve(file.read()))
+
+
+if __name__ == "__main__":
+    main()
