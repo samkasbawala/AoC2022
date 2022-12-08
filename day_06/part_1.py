8acc50fb5c7352d75fbaa4162d99197e69741bae
samkasbawala <samarth.kasbawala@uconn.edu>
2022-12-07 19:37:08 -0500

    day 6

---
+++ day_06/part_1.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import os
+import pytest
+
+INPUT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+INPUT_SAMPLE = """\
+mjqjpqmgbljsphdztnvjfqwrcgsmlb
+"""
+
+EXPECTED = 7
+
+
+def solve(input: str) -> int:
+    message = input.strip()
+
+    for idx in range(0, len(message) - 4):
+        sub_set = set(message[idx : idx + 4])
+        if len(sub_set) == 4:
+            return idx + 4
+
+    return -1
+
+
+@pytest.mark.parametrize(
+    ("input_sample", "expected"),
+    ((INPUT_SAMPLE, EXPECTED),),
+)
+def test(input_sample: str, expected: int):
+    assert solve(input_sample) == expected
+
+
+def main() -> None:
+    with open(INPUT, "r") as file:
+        print(solve(file.read()))
+
+
+if __name__ == "__main__":
+    main()
---
+++ day_06/part_2.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import os
+import pytest
+
+INPUT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+INPUT_SAMPLE = """\
+mjqjpqmgbljsphdztnvjfqwrcgsmlb
+"""
+
+EXPECTED = 19
+
+
+def solve(input: str) -> int:
+    message = input.strip()
+
+    for idx in range(0, len(message) - 14):
+        sub_set = set(message[idx : idx + 14])
+        if len(sub_set) == 14:
+            return idx + 14
+
+    return -1
+
+
+@pytest.mark.parametrize(
+    ("input_sample", "expected"),
+    ((INPUT_SAMPLE, EXPECTED),),
+)
+def test(input_sample: str, expected: int):
+    assert solve(input_sample) == expected
+
+
+def main() -> None:
+    with open(INPUT, "r") as file:
+        print(solve(file.read()))
+
+
+if __name__ == "__main__":
+    main()
